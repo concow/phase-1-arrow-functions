90581f40de5b99a2ad9926af503e102fbca3c45b
Connor Cowan <ccowan003@comcast.net>
2021-07-20 09:40:20 -0700

    commit tree f7b1b43c7de58f53dbcca5777d7cf903eddc06cb

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return (2000 / 100);
+}
+
+//function divide() {
+//   return (2000 / 100); 
+//}
+
+const square = (param) => param * param; //implicit return, 
+                                        //only situation doesn't require explicit return with the return keyword
+
+const add = (param1, param2) => param1 + param2;
